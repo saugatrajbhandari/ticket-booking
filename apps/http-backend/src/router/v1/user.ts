a59aa4c98a0f4ecd17f9b2036e5dbc8fdaee85eb
Saugat Rajbhandari <saugatrajbhandari47@gmail.com>
2025-03-03 23:58:04 +0545

    added signup endpoints db calls

--- apps/http-backend/src/router/v1/user.ts
+++ apps/http-backend/src/router/v1/user.ts
@@ -1,20 +1,39 @@
 import { Router } from "express";
 import authenticator from "authenticator";
+import { client } from "@repo/db/client";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "../../config";
 
 const router = Router();
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   console.log(req.body);
   const phoneNumber = req.body.phoneNumber;
 
   const result = authenticator.generateToken(phoneNumber + "signup");
   console.log(result);
 
+  const user = await client.user.upsert({
+    where: {
+      number: phoneNumber,
+    },
+    create: {
+      number: phoneNumber,
+    },
+    update: {},
+  });
+
+  if (process.env.NODE_ENV === "production") {
+    // send otp to user
+  }
+
   res.json({ result });
 });
 
-router.post("/signup/verify", (req, res) => {
+router.post("/signup/verify", async (req, res) => {
   const phoneNumber = req.body.phoneNumber;
+  const name = req.body.name;
+
   const otp = req.body.otp;
 
   const result = authenticator.verifyToken(phoneNumber + "signup", otp);
@@ -27,7 +46,24 @@ router.post("/signup/verify", (req, res) => {
     return;
   }
 
-  res.json({});
+  const userId = await client.user.update({
+    where: {
+      number: phoneNumber,
+    },
+    data: {
+      name,
+      verified: true,
+    },
+  });
+
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_PASSWORD
+  );
+
+  res.json({ token });
 
   console.log(result);
 
